18ae153b00e034215710df2a093d909c804b45c4
shantanupatil999 <119291452+shantanupatil999@users.noreply.github.com>
2023-01-30 09:52:53 +0530

    Add project files.

---
+++ smtpServer/Controllers/SetupController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Net.Mail;
+using smtpServer.Models;
+using System.Net.Configuration;
+namespace smtpServer.Controllers
+{
+    public class SetupController : Controller
+    {
+        // GET: Setup
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Save(smtpServer.Models.mail model)
+        {
+            try
+
+            {
+                MailMessage obj = new MailMessage(model.From, model.To);
+               
+                obj.Subject = model.Subject;
+                obj.Body = model.Body;
+                obj.IsBodyHtml = false;
+
+
+
+                SmtpClient smtp = new SmtpClient();
+
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+
+                smtp.EnableSsl = true;
+
+
+                NetworkCredential nc = new NetworkCredential("shaantanupatil999@gmail.com", "dcxxycauxituqcep");
+                smtp.UseDefaultCredentials = true;
+                smtp.Credentials = nc;
+                smtp.Send(obj);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+
+            return View();
+
+        }
+    }
+}
---
+++ smtpServer/ExternalJS/externalJS.js
@@ -0,0 +1,36 @@
+﻿const { post } = require("jquery");
+
+function validation()
+{
+    var to = $("#idto").val();
+    var subject = $("#idsubject").val();
+    var body = $("#idbody").val();
+    if (to == "") {
+        alert("Please Enter the valid data");
+    }
+    else if (subject == "") {
+        alert("Please Enter the Valid data");
+    }
+    else if (body == "") {
+        alert("Please Enter the Valid Data");
+    }
+    else {
+        alert("Email Sent Successfully!")
+    }
+}
+
+//function save() {
+
+
+//    var email_data = $("email_form").serialize()
+//    $.ajax({
+//        type: "post",
+//        url: "/Setup/Save",
+//        data: email_data,
+//        success: function (result) {
+//            alert("Email Sent Successfully!")
+//        }
+  
+
+//    })
+//}
